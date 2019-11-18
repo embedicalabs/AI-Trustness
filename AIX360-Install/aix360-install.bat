61f702d718254fb77a2ce45e86d6f7222ea00ca3
berkguldiken <berk.guldiken@gmail.com>
2019-11-18 13:56:20 +0300

    naming

--- AIX360-Install/aix360-install.bat
+++ AIX360-Install/aix360-install.bat
@@ -1,7 +1,7 @@
 set crpath=%CD%
 for /f "tokens=2*" %%a in ('REG QUERY "HKEY_CURRENT_USER\Software\Python\ContinuumAnalytics\Anaconda37-64\InstallPath" /ve') do set "regpath=%%~b"
 @RD /S /Q "%regpath%\envs\aix"
-bitsadmin /transfer MyJob /priority FOREGROUND "http://github.com/IBM/AIX360/archive/master.zip" "C:\Users\%USERNAME%\Downloads\aix.zip"
+bitsadmin /transfer aix/priority FOREGROUND "http://github.com/IBM/AIX360/archive/master.zip" "C:\Users\%USERNAME%\Downloads\aix.zip"
 powershell.exe -NoP -NonI -Command "Expand-Archive 'C:\Users\%USERNAME%\Downloads\aix.zip' '%crpath%'"
 call %regpath%\Scripts\activate.bat %regpath%
 call conda create --name aix python=3.6 -y
@@ -10,7 +10,7 @@ rem call C:\Users\Berk\AppData\Local\Continuum\anaconda3\Scripts\activate.bat C:
 pip install torch==1.3.1+cpu torchvision==0.4.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
 rem "C:\Users\%USERNAME%\AppData\Local\Continuum\anaconda3\python.exe" "%crpath%\AIX360-master\install\install.py"
 cd %crpath%\AIX360-master
-bitsadmin /transfer mydownloadjob /download /priority normal https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe C:\Users\%USERNAME%\Downloads\visualcppbuildtools_full.exe
+bitsadmin /transfer visualcppdownload /download /priority normal https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe C:\Users\%USERNAME%\Downloads\visualcppbuildtools_full.exe
 cd C:\Users\%USERNAME%\Downloads
 start visualcppbuildtools_full.exe
 echo "please install the build tool. After installing press any button to continue" 
--- Adversarial-Robustness-Install/air-install.bat
+++ Adversarial-Robustness-Install/air-install.bat
@@ -1,13 +1,13 @@
 set crpath=%CD%
 for /f "tokens=2*" %%a in ('REG QUERY "HKEY_CURRENT_USER\Software\Python\ContinuumAnalytics\Anaconda37-64\InstallPath" /ve') do set "regpath=%%~b"
 @RD /S /Q "%regpath%\envs\air"
-bitsadmin /transfer MyJob /priority FOREGROUND "https://github.com/IBM/adversarial-robustness-toolbox/archive/master.zip" "C:\Users\%USERNAME%\Downloads\air.zip"
+bitsadmin /transfer air /priority FOREGROUND "https://github.com/IBM/adversarial-robustness-toolbox/archive/master.zip" "C:\Users\%USERNAME%\Downloads\air.zip"
 powershell.exe -NoP -NonI -Command "Expand-Archive 'C:\Users\%USERNAME%\Downloads\air.zip' '%crpath%'"
 call %regpath%\Scripts\activate.bat %regpath%
 call conda create --name air python=3.6 -y
 call conda activate air
 cd %crpath%\adversarial-robustness-toolbox-master
-bitsadmin /transfer mydownloadjob /download /priority normal https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe C:\Users\%USERNAME%\Downloads\visualcppbuildtools_full.exe
+bitsadmin /transfer visualcppdownload /download /priority normal https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe C:\Users\%USERNAME%\Downloads\visualcppbuildtools_full.exe
 cd C:\Users\%USERNAME%\Downloads
 start visualcppbuildtools_full.exe
 echo "please install the build tool. After installing press any button to continue" 
