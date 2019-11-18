8280ea739de89a331f22c4b7ce2c7ecf75dc555a
berkguldiken <berk.guldiken@gmail.com>
2019-11-18 14:14:24 +0300

    Update aif360-install.bat

--- AIF360-Install/aif360-install.bat
+++ AIF360-Install/aif360-install.bat
@@ -8,8 +8,8 @@ call conda create --name aif python=3.6 -y
 call conda activate aif
 cd %crpath%\AIF360-master
 pause
-bitsadmin /transfer visualcppdownload /download /priority normal https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe %crpath%\visualcppbuildtools_full.exe
-cd %crpath%
+bitsadmin /transfer visualcppdownload /download /priority normal https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe "C:\Users\%USERNAME%\Downloads\visualcppbuildtools_full.exe"
+cd C:\Users\%USERNAME%\Downloads
 start visualcppbuildtools_full.exe
 echo "please install the build tool. After installing press any button to continue" 
 PAUSE
